d887afe4414ae29af6786519b98fcf528f664f29
ahjee12 <ahjee200@gmail.com>
2022-09-10 17:03:54 +0900

    2022.09.10

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 // @ts-check
 
 // github 레포지토리 관리 CLI 만들기
+// 환경변수 이용하지 않고 .env 파일에 넣어 관리 가능
+require('dotenv').config()
+
+// const { GITHUB_ACCESS_TOKEN } = process.env
 const { Command } = require('commander')
 
 const program = new Command()
@@ -10,3 +14,5 @@ program
   .name('string-util')
   .description('CLI to some JavaScript string utilities')
   .version('0.8.0')
+
+program.parse()
